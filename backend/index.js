1bef3b231ff9e991a69e60f2e7e3868a8ea174a7
divyadharshana5 <divyadharshana3@gmail.com>
2024-09-17 21:03:08 +0530

    Save  a new book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,37 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookmodel.js";
 
 const app = express();
+
+app.use(express.json());
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome To MERN Stack Tutorial");
+});
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields:title,author,publishYear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
--- backend/models/bookmodel.js
+++ backend/models/bookmodel.js
@@ -19,4 +19,4 @@ const bookSchema = mongoose.Schema(
     timestamps: true,
   }
 );
-export const Book = mongoose.model("Cat", bookSchema);
+export const Book = mongoose.model("Books", bookSchema);
