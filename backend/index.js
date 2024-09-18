596f2b93253e717ccd5fcdcc5b46439132201620
divyadharshana5 <divyadharshana3@gmail.com>
2024-09-18 18:49:29 +0530

    Get all books with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookmodel.js";
@@ -28,6 +28,19 @@ app.post("/books", async (request, response) => {
     };
     const book = await Book.create(newBook);
     return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+app.get(`/books`, async (request, response) => {
+  try {
+    const books = await Book.find({});
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
   } catch (error) {
     console.log(error.message);
     response.status(500).send({ message: error.message });
